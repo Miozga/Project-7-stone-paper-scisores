4ad579537bf40892c0294592b42074d885b6b8e6
Sebastian Miozga <miozga.sebastian@gmail.com>
2025-02-18 16:38:54 +0100

    abc

--- game.js
+++ game.js
@@ -13,10 +13,42 @@ const game = {
 const hands = [...document.querySelectorAll(".select img")];
 
 function handSelection() {
-  game.playerHand = this.dataset.option;
+  game.playerHand = this.dataset.option; // przypisujemy do const game
   console.log(game.playerHand);
 
-  this.style.boxShadow = "0 0 0 4px yellow";
+  hands.forEach((hand) => (hand.style.boxShadow = ""));
+  this.style.boxShadow = "0 0 0 4px blue";
+}
+
+function aiChoice() {
+  return hands[Math.floor(Math.random() * 3)].dataset.option;
+}
+
+//sprawdzamy wniki gracza i komputera
+function checkResult(player, ai) {
+  if (player === ai) {
+    console.log("remis");
+  } else if (
+    (player === "papier" && ai === "kamień") ||
+    (player === "kamień" && ai === "nożyczki") ||
+    (player === "nożyczki" && ai === "papier")
+  ) {
+    console.log("wygrałeś");
+  } else {
+    console.log("przegrałeś");
+  }
+}
+
+//funkcja sterująca
+function startGame() {
+  if (!game.playerHand) {
+    return alert("Wybierz dłoń");
+  }
+
+  game.aiHand = aiChoice();
+  const gameResult = checkResult(game.playerHand, game.aiHand);
 }
 
 hands.forEach((hand) => hand.addEventListener("click", handSelection));
+
+document.querySelector(".start").addEventListener("click", startGame);
